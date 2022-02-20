c88c646db22e0106b71a117b5dfd7c157399bebe
Léo Boyer <contact@leoboyer.fr>
2022-02-20 10:12:11 +0100

    Admin layout base

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -14,4 +14,13 @@ class AdminController extends AbstractController {
     public function dashboard(): Response {
         return $this->render('admin/dashboard.html.twig');
     }
+    #[Route('/dates', name: 'admin_dates')]
+    public function dates(): Response {
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    protected function render(string $view, array $parameters = [], Response $response = null): Response {
+        $parameters['isAdmin'] = true;
+        return parent::render($view, $parameters, $response);
+    }
 }
