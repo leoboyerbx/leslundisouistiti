ac1590364f44c3d4cf582579a1899c4592db6139
Léo Boyer <contact@leoboyer.fr>
2022-02-28 21:22:23 +0100

    Site options

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,8 +1,12 @@
 <?php
 namespace App\Controller;
 
+use App\Options\OptionsManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +15,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController {
 
     #[Route('', name: 'admin_dashboard')]
-    public function dashboard(): Response {
-        return $this->render('admin/dashboard.html.twig');
+    public function dashboard(Request $request, OptionsManager $optionsManager): Response {
+        $siteTitle = $optionsManager->getOption('site_title');
+        $siteDescription = $optionsManager->getOption('site_description');
+
+        $options = ['title' => $siteTitle, 'description' => $siteDescription];
+        $form = $this->createFormBuilder($options)
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+            $optionsManager->setOption('site_title', $data['title']);
+            $optionsManager->setOption('site_description', $data['description']);
+        }
+
+
+        return $this->renderForm('admin/dashboard.html.twig', [
+            'form' => $form
+        ]);
     }
     #[Route('/dates', name: 'admin_dates')]
     public function dates(): Response {
